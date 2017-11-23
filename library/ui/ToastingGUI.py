b44546dcd25688098f8d7a159be51674017c03a2
Chip <imchipwood@gmail.com>
2017-11-23 15:39:37 -0800

    auto size maybe?

--- library/ui/ToastingGUI.py
+++ library/ui/ToastingGUI.py
@@ -529,6 +529,7 @@ class ToastingGUI(ToastingBase):
 
 			# Set column label (step name)
 			self.configurationGrid.SetColLabelValue(colNum, stepName)
+			self.configurationGrid.SetColMinimalWidth(colNum, 100)
 
 			# Insert config values
 			targetTemp = str(self.stateConfiguration[stepName][CONFIG_KEY_TARGET])
@@ -536,7 +537,7 @@ class ToastingGUI(ToastingBase):
 			self.configurationGrid.SetCellValue(col=colNum, row=0, s=targetTemp)
 			self.configurationGrid.SetCellValue(col=colNum, row=1, s=stepDuration)
 
-		self.configurationGrid.AutoSizeColumns()
+		# self.configurationGrid.AutoSizeColumns()
 
 	@decorators.BusyReady(MODEL_NAME)
 	def configurationGridOnGridCellChange(self, event):
