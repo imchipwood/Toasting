67127a669f6db195bcbb2e09f61fea20de2fb378
Chip <imchipwood@gmail.com>
2017-11-26 02:21:56 -0800

    heck

--- library/ui/ToastingGUI.py
+++ library/ui/ToastingGUI.py
@@ -268,7 +268,7 @@ class ToastingGUI(ToastingBase):
 		@param visualizer: matplotlib configurationVisualizer for displaying config setup
 		@type visualizer: ConfigurationVisualizer
 		"""
-		# self.logger.debug("redrawConfigurationVisualization")
+		self.logger.debug("redrawConfigurationVisualization START")
 		# Clear out existing configurationVisualizer
 		sizer = self.configurationVisualizerPanel.GetSizer()
 		sizer.Clear()
@@ -279,6 +279,7 @@ class ToastingGUI(ToastingBase):
 		canvas = FigureCanvas(self.configurationVisualizerPanel, -1, visualizer.fig)
 		sizer.Add(canvas, 1, wx.EXPAND)
 		self.configurationVisualizerPanel.Layout()
+		self.logger.debug("redrawConfigurationVisualization FINISH")
 
 	@decorators.BusyReady(MODEL_NAME)
 	def redrawLiveVisualization(self, visualizer):
