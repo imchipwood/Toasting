1143c9578df49f34906c4a92410feea403ed3f84
Charles Wood <imchipwood@gmail.com>
2018-08-29 19:18:10 -0700

    little more testing for the config

--- tests/other/test_config.py
+++ tests/other/test_config.py
@@ -26,4 +26,28 @@ def test_ToasterConfig():
 	assert isinstance(config.spiCsPin, int)
 	assert isinstance(config.relayPin, int)
 	assert isinstance(config.clockPeriod, (float, int))
-	
+
+
+def test_NonDefaults(monkeypatch):
+	"""
+	Test that the configuration was actually loaded by monkeypatching a bunch of
+	crap into the ToasterConfig class
+	"""
+	fakeUnits = "some fake value"
+	fakePins = {
+		"SPI_CS": 12341,
+		"relay": 234234
+	}
+	fakePeriod = -10
+	fakePID = "not a PID object"
+	monkeypatch.setattr(ToasterConfig, 'BASE_UNITS', fakeUnits)
+	monkeypatch.setattr(ToasterConfig, 'BASE_PINS', fakePins)
+	monkeypatch.setattr(ToasterConfig, 'BASE_CLOCK_PERIOD', fakePeriod)
+	monkeypatch.setattr(ToasterConfig, 'BASE_PID', fakePID)
+
+	config = ToasterConfig(GetBaseConfigurationFilePath())
+	assert config.units != fakeUnits
+	assert config.pins != fakePins
+	assert config.clockPeriod != fakePeriod
+	assert config.pids != fakePID
+	assert isinstance(config.pids, PID)
