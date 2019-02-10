c5393d32c37c01276e298bb0cf944166d2344bec
Chip <imchipwood@gmail.com>
2019-02-10 13:05:59 -0800

    added ability to send config if requested

--- library/communication/async_websocket.py
+++ library/communication/async_websocket.py
@@ -2,17 +2,52 @@ import asyncio
 import websockets
 import json
 
+from definitions import GetBaseConfigurationFilePath
+from library.control.stateMachine import ToastStateMachine
+
+model = ToastStateMachine(jsonConfigPath=GetBaseConfigurationFilePath())
+
 # https://stackoverflow.com/questions/45002490/how-to-receive-data-from-web-sockets-using-python
+
+
+def get_flattened_tuning():
+	config = model.config
+	return {
+		'kP': config.pids.kP,
+		'kI': config.pids.kI,
+		'kD': config.pids.kD,
+		'outputMin': config.pids.min,
+		'outputMax': config.pids.max,
+		'windupGuard': config.pids.windupGuard,
+		'timerPeriod': config.clockPeriod,
+		'pinRelay': config.relayPin,
+		'pinSpiCs': config.spiCsPin,
+	}
 
 
 async def hello(websocket, path):
 	data = await websocket.recv()
 	data = json.loads(data)
-	print(json.dumps(data, indent=2))
-	await websocket.send("message {} received".format(data.get('id', "NONE")))
+	message_id = data.get('id', 'NONE')
+	message_type = data.get('type')
+	message_method = data.get('method')
+	message_text = data.get('text')
+	print("{} {} of type {} received".format(message_method, message_id, message_type))
+	print(json.dumps(message_text, indent=2))
+
+	if message_method == 'GET':
+		if message_type == 'tuning':
+			msg = {
+				'tuning': get_flattened_tuning(),
+				'ACK': message_id
+			}
+			await websocket.send(json.dumps(msg))
+	else:
+		await websocket.send(json.dumps({'ACK': message_id}))
 
 
 HOST, PORT = 'localhost', 9999
+# HOST, PORT = 'localhost', 3000
 start_server = websockets.serve(hello, HOST, PORT)
 
 try:
